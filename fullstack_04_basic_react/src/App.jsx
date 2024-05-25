f2d2c24ee07d4157a788bffea9572f85feb217d4
tamar edri <tedri@g.jct.ac.il>
2024-05-25 22:12:59 +0300

    added buttons to navigate through the projects

--- fullstack_04_basic_react/src/App.jsx
+++ fullstack_04_basic_react/src/App.jsx
@@ -4,25 +4,50 @@ import Game100 from './Game100Components/Game100';
 
 
 function App() {
- 
-    const [currentComponent, setCurrentComponent] = useState('TextEditor');
-    //  {currentComponent === 'TextEditor' && <TextEditor/>}
 
-    //   {currentComponent === 'Game100' && <Game100 users={users}/>}
+  const [currentComponent, setCurrentComponent] = useState('Select');
+  //  {currentComponent === 'TextEditor' && <TextEditor/>}
 
+  //   {currentComponent === 'Game100' && <Game100 users={users}/>}\
+  let show = false;
   return (
 
+    <>
 
-    <Game100 />
+      {currentComponent === 'Select' &&
+        <>
+          <button onClick={() => setCurrentComponent('TextEditor')}>⌨️</button>
+          <button onClick={() => setCurrentComponent('Game100')}>💯</button>
+        </>
+      }
 
-    // <TextEditor />
+      {currentComponent === 'TextEditor' &&
+        <>
+          <button onClick={() => setCurrentComponent('Select')}>🏠</button>
+          <TextEditor />
+        </>
+      }
+
+
+      {
+        currentComponent === 'Game100' &&
+        <>
+          <button onClick={() => setCurrentComponent('Select')}>🏠</button>
+          <Game100 />
+        </>
+      }
+
+
+    </>
+
+    // 
     // <div>
     //   <div>
     //     <button onClick={() => setCurrentComponent('TextEditor')}>Text Editor</button>
     //     <button onClick={() => setCurrentComponent('Game100')}>GameBoard</button>
     //   </div>
-    
-  // </div>
+
+    // </div>
   )
 }
 
